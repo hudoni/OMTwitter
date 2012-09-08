ae8dd1b11a6efd21c4adcee10ff4be3ac856951c
Sangwon Park <hudoni@gmail.com>
2012-09-08 02:46:57 +0200

    Defined TweetFilter interface, and created FilterUserName that implemented the interface.

--- omtwitter-core/src/main/java/com/maalaang/omtwitter/corpus/FilterUserName.java
+++ omtwitter-core/src/main/java/com/maalaang/omtwitter/corpus/FilterUserName.java
@@ -3,90 +3,79 @@
  */
 package com.maalaang.omtwitter.corpus;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+import com.maalaang.omtwitter.model.OMTweet;
+
 /**
  * @author Sangwon Park
  *
  */
 public class FilterUserName implements TweetFilter {
-/*
-	public void tweetCorpusUserFilterList(TwitterCorpusReader reader, String out, int threshold) {
-		try {
-			HashMap<String,Integer> userFreqMap = new HashMap<String, Integer>();
-			
-			while (reader.hasNext()) {
-				reader.next();
-				String user = reader.getUser();
-				if (user == null) {
-					continue;
-				}
-				
-				Integer value = userFreqMap.get(user);
-				if (value != null) {
-					userFreqMap.put(user, value + 1);
-				} else {
-					userFreqMap.put(user, 1);
-				}
-			}
-			
-			FileWriter fw = new FileWriter(out);
-			TreeSet<Entry<String,Integer>> set = new TreeSet<Entry<String,Integer>>(new Comparator<Entry<String,Integer>>() {
-				@Override
-				public int compare(Entry<String, Integer> arg0, Entry<String, Integer> arg1) {
-					int diff = arg1.getValue() - arg0.getValue();
-					if (diff == 0) {
-						return arg1.getKey().compareTo(arg0.getKey());
-					}
-					return diff;
-				}
-			});
-			set.addAll(userFreqMap.entrySet());
-			
-			for (Entry<String, Integer> entry : set) {
-				if (entry.getValue() > threshold) {
-					fw.write(entry.getKey());
-					fw.write('\n');
-				}
-			}
-			fw.close();
-			
-			logger.info("user filtering list was written on '" + out + "' (threashold=" + threshold + ")");
-			
-		} catch (IOException e) {
-			e.printStackTrace();
+	
+	private HashMap<String,Integer> userPostFreqMap = null;
+	private LinkedList<String> userNameQueue = null;
+	private int windowSize = 0;
+	private int userPostLimit = 0;
+	private boolean filtered = false;
+	private String filterName = null;
+	
+	public FilterUserName(int windowSize, int userPostLimit, String filterName) {
+		this.windowSize = windowSize;
+		this.userPostLimit = userPostLimit;
+		this.filterName = filterName;
+	}
+
+	public void initialize() {
+		if (windowSize > 0) {
+			userPostFreqMap = new HashMap<String,Integer>(windowSize);
+			userNameQueue = new LinkedList<String>();
+		} else {
+			userPostFreqMap = new HashMap<String,Integer>();
 		}
 	}
 	
-	public void tweetCorpusFilterByUser(TwitterCorpusReader reader, String out, String userList) {
-		try {
-			Set<String> userFilterSet = CollectionTextReader.readSet(userList);
-			
-			FileWriter fw = new FileWriter(out);
-			
-			while (reader.hasNext()) {
-				reader.next();
-				
-				String user = reader.getUser();
-				String query = reader.getQuery();
-				if (user != null && !userFilterSet.contains(user)) {
-					if (query != null) {
-						fw.write(query);
-						fw.write('\t');
-					}
-					fw.write(user);
-					fw.write('\t');
-					fw.write(reader.getText());
-					fw.write('\n');
-//				} else {
-//					logger.info("filted by user: " + user + "\t" + reader.getText());
+	public void next(OMTweet tweet) {
+		String userName = tweet.getAuthor();
+		Integer freq = null;
+		
+		if (windowSize > 0) {
+			userNameQueue.addLast(userName);
+			if (userNameQueue.size() > windowSize) {
+				String removedUserName = userNameQueue.pollFirst();
+				freq = userPostFreqMap.get(removedUserName);
+				if (freq == null) {
+					throw new IllegalStateException();
+				} else {
+					userPostFreqMap.put(removedUserName, --freq);
 				}
 			}
-			
-			fw.close();
-			logger.info("'" + out + "' filtered by user has been created (filtered by '" + userList + "')");
-			
-		} catch (IOException e) {
-			e.printStackTrace();
+		}
+		
+		freq = userPostFreqMap.get(userName);
+		if (freq == null) {
+			filtered = userPostLimit > 0 ? false : true;
+			userPostFreqMap.put(userName, 1);
+		} else {
+			filtered = userPostLimit > freq ? false : true; 
+			userPostFreqMap.put(userName, ++freq);
 		}
 	}
-	*/
+
+	public boolean isFilteredOut() {
+		return filtered;
+	}
+
+	public void close() {
+		userPostFreqMap.clear();
+		userPostFreqMap = null;
+		userNameQueue.clear();
+		userNameQueue = null;
+	}
+	
+	public String getFilterName() {
+		return filterName;
+	}
 }
--- omtwitter-core/src/main/java/com/maalaang/omtwitter/corpus/TweetFilter.java
+++ omtwitter-core/src/main/java/com/maalaang/omtwitter/corpus/TweetFilter.java
@@ -3,10 +3,22 @@
  */
 package com.maalaang.omtwitter.corpus;
 
+import com.maalaang.omtwitter.model.OMTweet;
+
 /**
  * @author Sangwon Park
  *
  */
 public interface TweetFilter {
+	
+	public void initialize();
+	
+	public void next(OMTweet tweet);
+	
+	public boolean isFilteredOut();
+	
+	public String getFilterName();
+	
+	public void close();
 
 }
