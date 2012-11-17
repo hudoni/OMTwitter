048a08dc04fae98f7fe03bcff854ae25cb6d9cee
Sangwon Park <hudoni@gmail.com>
2012-11-17 19:20:20 +0900

    Added empty unit test

---
+++ jnisvmlight/src/test/java/jnisvmlight/SVMLightInterfaceTest.java
@@ -0,0 +1,20 @@
+package jnisvmlight;
+
+import junit.framework.TestCase;
+
+public class SVMLightInterfaceTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testTrainModelLabeledFeatureVectorArray() {
+	}
+
+	public void testTrainModelLabeledFeatureVectorArrayStringArray() {
+	}
+
+	public void testTrainModelLabeledFeatureVectorArrayTrainingParameters() {
+	}
+
+}
