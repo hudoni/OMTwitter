1955596cd517526165fbc81146ffc49cf07a5395
Sangwon Park <hudoni@gmail.com>
2012-11-19 01:53:48 +0900

    deleted backup directories

--- omtwitter/backup/run/run.bat
+++
@@ -1,3 +0,0 @@
-# mvn exec:java -Dexec.mainClass="com.maalaang.omtwitter.App"
-
-mvn exec:java -Dexec.mainClass="com.maalaang.omtwitter.demo.OMTwitterWorkflow" -Dorg.apache.uima.logger.class="org.apache.uima.util.impl.Log4jLogger_impl"
