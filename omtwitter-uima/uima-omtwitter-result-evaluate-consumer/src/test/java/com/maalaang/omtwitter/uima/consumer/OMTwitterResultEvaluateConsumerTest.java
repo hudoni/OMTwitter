44e860de784d061e945692e122ad10672c14d71a
Sangwon Park <hudoni@gmail.com>
2012-12-26 05:29:20 +0900

    Empty unit test added

---
+++ omtwitter-uima/uima-omtwitter-result-evaluate-consumer/src/test/java/com/maalaang/omtwitter/uima/consumer/OMTwitterResultEvaluateConsumerTest.java
@@ -0,0 +1,15 @@
+package com.maalaang.omtwitter.uima.consumer;
+
+import junit.framework.TestCase;
+
+public class OMTwitterResultEvaluateConsumerTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
